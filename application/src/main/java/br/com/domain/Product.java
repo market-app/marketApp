6452801e7e1d3509638b6335bc1d3f576009354d
joao.venancio <joao.venancio@mv.com.br>
2023-03-01 21:41:03 -0300

    domain created

---
+++ application/src/main/java/br/com/domain/Product.java
@@ -0,0 +1,23 @@
+package br.com.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String nome;
+}
