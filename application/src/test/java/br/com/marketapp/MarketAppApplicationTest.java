3fb4d362f256b332224e02624092bdfdbbbfb7f7
Leonardo Evangelista de Araújo <leonardo.araujo@mv.com.br>
2023-03-09 13:12:27 -0300

    [MA-24] Ajustar os testes

---
+++ application/src/test/java/br/com/marketapp/MarketAppApplicationTest.java
@@ -0,0 +1,17 @@
+package br.com.marketapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketAppApplicationTest {
+
+    @Test
+    void main() {
+        MarketAppApplication.main(new String[] {});
+    }
+
+    @Test
+    void configure() {
+    }
+}
--- product/src/main/java/br/com/marketapp/product/domain/Product.java
+++ product/src/main/java/br/com/marketapp/product/domain/Product.java
@@ -11,7 +11,6 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
