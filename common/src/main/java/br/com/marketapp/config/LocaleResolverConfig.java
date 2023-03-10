58a2af254700ba95c6dc176acbcaa94b0e80fca4
Bruna Santos <brunaguimaraes.santos@hotmail.com>
2023-03-10 07:59:06 -0300

    [MA-26] Fazendo a internacionalização

---
+++ common/src/main/java/br/com/marketapp/config/LocaleResolverConfig.java
@@ -0,0 +1,42 @@
+package br.com.marketapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+public class LocaleResolverConfig extends AcceptHeaderLocaleResolver {
+
+    List<Locale> LOCALES = Arrays.asList(
+            new Locale("en"),
+            new Locale("pt", "BR")
+    );
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String headLang = request.getHeader("Accept-Language");
+        return headLang == null || headLang.isEmpty()
+                ? Locale.ENGLISH
+                : Locale.lookup(Locale.LanguageRange.parse(headLang), LOCALES);
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rs = new ResourceBundleMessageSource();
+        rs.addBasenames(
+                "messages-application",
+                "messages-common"
+        );
+                rs.setUseCodeAsDefaultMessage(true);
+        rs.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        return rs;
+
+    }
+}
